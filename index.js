c0462b105f6f121c48fb093ae1e91f2011b031dc
Pahal Jain <77042471+pahaljain@users.noreply.github.com>
2024-08-08 17:30:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import connectDB from "./config/db.js";
 const app = express();
 
 // Middleware
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: ["http://localhost:5173","https://frontend-event-planner.vercel.app"], credentials: true }));
 app.use(express.json());
 
 // Routes
